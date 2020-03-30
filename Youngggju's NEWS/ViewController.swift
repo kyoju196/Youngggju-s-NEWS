0a7221205077e5b50f78bc90d65769255ff9dc52
Kwon YoungJu <kyoju196@gmail.com>
2020-03-30 17:45:24 +0900

    Initial Commit

---
+++ Youngggju's NEWS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youngggju's NEWS
+//
+//  Created by 권영주 on 2020/03/30.
+//  Copyright © 2020 권영주. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
